ea7eaece01822586c273cfc5b9dcff62833d202f
codingguh <teguhharits91@gmail.com>
2024-10-21 05:34:29 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/test_efea/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.test_efea
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screen/home.dart
@@ -0,0 +1,190 @@
+import 'package:flutter/material.dart';
+import 'package:test_efea/api/repository.dart';
+import 'package:test_efea/screen/add_edit_post_screen.dart';
+import 'package:test_efea/widgets/post_view.dart';
+import 'package:test_efea/model/post.dart';
+
+class Home extends StatefulWidget {
+  final String title;
+
+  const Home({
+    super.key,
+    required this.title,
+  });
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final ScrollController _scrollController = ScrollController();
+  final Repository _apiService = Repository();
+
+  final List<Post> _posts = [];
+  bool _isLoading = false;
+  bool _hasMore = true;
+  int _currentPage = 1;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadPosts();
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        _loadMorePosts();
+      }
+    });
+  }
+
+  Future<void> _deletePost(int postId) async {
+    try {
+      final response = await _apiService.deletePost(postId);
+      if (response) {
+        setState(() {
+          _posts.removeWhere((post) => post.id == postId);
+        });
+        _loadMorePosts();
+      }
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+
+  Future<void> _loadPosts() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    try {
+      final result = await _apiService.fetchPosts(_currentPage);
+      setState(() {
+        _currentPage++;
+        _posts.clear(); // Clear old posts for a fresh load
+        _posts.addAll(result['posts']);
+        _hasMore = result['nextPageUrl'] != null;
+      });
+    } catch (e) {
+      throw Exception(e.toString());
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
+  Future<void> _loadMorePosts() async {
+    if (_isLoading || !_hasMore) return;
+
+    setState(() {
+      _isLoading = true;
+    });
+
+    try {
+      final result = await _apiService.fetchPosts(_currentPage);
+      setState(() {
+        _currentPage++;
+        _posts.addAll(result['posts']);
+        _hasMore = result['nextPageUrl'] != null;
+      });
+    } catch (e) {
+      throw Exception(e.toString());
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: RefreshIndicator(
+        onRefresh: _loadPosts, // Trigger refresh on pull down
+        child: _buildPostList(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          //TODO action to add and edit post
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const AddEditPostScreen(),
+            ),
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+
+  Widget _buildPostList() {
+    return ListView.builder(
+      controller: _scrollController,
+      itemCount: _posts.length + (_hasMore ? 1 : 0),
+      itemBuilder: (context, index) {
+        if (index == _posts.length) {
+          return const Center(child: CircularProgressIndicator());
+        }
+        final post = _posts[index];
+
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Card.filled(
+            clipBehavior: Clip.hardEdge,
+            child: InkWell(
+              onTap: () {
+                // Todo action ke detail post
+              },
+              child: Stack(
+                children: [
+                  PostView(post: post),
+                  Positioned(
+                    top: 2,
+                    right: 2,
+                    child: IconButton(
+                      onPressed: () async {
+                        final result = await Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => AddEditPostScreen(post: post),
+                          ),
+                        );
+
+                        if (result == true) {
+                          _currentPage = 1;
+                          _posts.clear();
+                          _hasMore = true;
+                          _loadPosts();
+                        }
+                      },
+                      icon: const Icon(Icons.edit_rounded),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 2,
+                    right: 2,
+                    child: IconButton(
+                      onPressed: () => _deletePost(post.id),
+                      icon: const Icon(Icons.delete_rounded),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+}
